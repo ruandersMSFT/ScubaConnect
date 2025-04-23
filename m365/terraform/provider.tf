3cfe724323e4fbe8c817f722dab8d3653f37afaa
ruandersMSFT <russell@russellanderson.net>
2025-04-22 22:29:57 -0600

    required_version

--- m365/terraform/provider.tf
+++ m365/terraform/provider.tf
@@ -3,15 +3,15 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.26"
+      version = "~> 4.26.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "~> 3.3"
+      version = "~> 3.3.0"
     }
   }
 
-  required_version = ">= 1.1.0"
+  required_version = ">= 1.11.4"
 }
 
 provider "azurerm" {
